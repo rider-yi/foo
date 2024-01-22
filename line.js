530206f05279ff8f41a2764b11f72d0a8a7e1ebe
Yuki Ishikawa <rideryi89@gmail.com>
2024-01-22 13:38:24 +0900

    fix

--- line.js
+++ line.js
@@ -1,10 +1,11 @@
-function addLineOverlay() {
+function addLineOverlay(a8mat) {
   const elm = document.createElement("div");
   elm.setAttribute(
     "style",
     "position: fixed; z-index: 999999; padding: 0; margin: 0; right: 10px; bottom: 10px; height: 70px; width: 70px; background-color: rgba(0, 0, 0, 0); background-image: url(https://raw.githubusercontent.com/rider-yi/foo/main/line-icon.webp); background-size: cover; cursor: pointer;"
   );
-  elm.onclick = () => window.open("https://www.fancs.com", "_blank");
+  elm.onclick = () =>
+    window.open(`https://www.fancs.com?a8mat=${a8mat}`, "_blank");
 
   const body = document.getElementsByTagName("body").item(0);
   if (!body) {
@@ -15,10 +16,10 @@ function addLineOverlay() {
 }
 
 // スクリプトタグからパラメータを取得
-const nineApiKey = document.currentScript.getAttribute("apiKey");
-if (!nineApiKey) {
-  console.log("The apiKey attribute is required.");
+const a8mat = document.currentScript.getAttribute("a8mat");
+if (!a8mat) {
+  console.log("The a8mat attribute is required.");
 } else {
-  console.log("apiKey:", nineApiKey);
-  document.addEventListener("DOMContentLoaded", addLineOverlay);
+  console.log("a8mat:", a8mat);
+  document.addEventListener("DOMContentLoaded", () => addLineOverlay(a8mat));
 }
